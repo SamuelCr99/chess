9b01deaaaeec36c681622c25d36808327780e246
Samuel Collier Ryder <samuel.collier.ryder@gmail.com>
2022-12-30 07:37:08 +0100

    Added some more logic

--- classes.h
+++ classes.h
@@ -7,6 +7,13 @@ class Piece {
         string color;
         int row; 
         int col; 
+        bool legalMove(int currRow, int currCol, int nextRow, int nextCol){
+            if (currCol == nextCol && currRow-nextRow <= 2 && currRow-nextRow > 0){
+                return true;
+            }
+            return false;
+        }
+
 };
 
 class Pawn : public Piece{
@@ -16,6 +23,29 @@ class Pawn : public Piece{
             row = r; 
             col = co;
         }
+        bool legalMove(int currRow, int currCol, int nextRow, int nextCol){
+            if (currCol == nextCol && currRow-nextRow <= 2 && currRow-nextRow > 0){
+                return true;
+            }
+            return false;
+        
+        }
+};
+
+class Rook : public Piece{
+    public:
+        Rook(string c, int r, int co){
+            color = c;
+            row = r; 
+            col = co;
+        }
+        // bool legalMove(int currRow, int currCol, int nextRow, int nextCol){
+        //     if (currCol == nextCol || currRow == nextRow){
+        //         return true;
+        //     }
+        //     return false;
+        
+        // }
 };
 
 class Square {
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include "classes.h"
+#include <math.h>
 
 using namespace std;
 
@@ -20,13 +21,21 @@ void drawSquares(sf::RenderWindow& w, vector<vector <Square>>& sm){
     }
 }
 
-void drawPieces(sf::RenderWindow& w, Pawn& p){
+void drawPieces(sf::RenderWindow& w, vector<vector<Square>> sm){
     sf::Texture t; 
-    t.loadFromFile("Textures/w_pawn.png");
-    sf::Sprite Sprite(t);
-    Sprite.setPosition(p.col*100 + 20,p.row*100 + 15);
-    Sprite.setScale(0.6,0.6);
-    w.draw(Sprite);
+    sf::Sprite Sprite;
+
+    for (int col = 0; col < 8; col++){
+        for (int row = 0; row < 8; row++){
+            if (sm[row][col].p.color == "white"){
+                t.loadFromFile("Textures/w_pawn.png");
+                Sprite.setTexture(t);
+                Sprite.setPosition(sm[row][col].p.col*100 + 18,sm[row][col].p.row*100 + 15);
+                Sprite.setScale(0.6,0.6);
+                w.draw(Sprite);
+            }
+        }
+    }
 }
 
 void buildMatrix(vector <vector <Square>>& sm){
@@ -48,25 +57,64 @@ void buildMatrix(vector <vector <Square>>& sm){
     }
 }
 
+Piece& selectPiece(vector<vector <Square>>& sm, sf::RenderWindow & w){
+    sf::Vector2i mousePos = sf::Mouse::getPosition(w);
+    int pressedCol = floor(mousePos.x / 100);
+    int pressedRow = floor(mousePos.y / 100);
+    return sm[pressedRow][pressedCol].p;
+}
+
+void movePiece(vector<vector <Square>>& sm, sf::RenderWindow& w, Piece& p){
+    sf::Vector2i mousePos = sf::Mouse::getPosition(w);
+    int pressedCol = floor(mousePos.x / 100);
+    int pressedRow = floor(mousePos.y / 100);
+    if (p.legalMove(p.row,p.col,pressedRow, pressedCol)){
+        sm[p.row][p.col].p = Piece();
+        p.col = pressedCol;
+        p.row = pressedRow;
+        sm[pressedRow][pressedCol].p = p;
+    }
+}
+
 
 int main(){
     srand(time(NULL));
     sf::RenderWindow window(sf::VideoMode(800, 800), "Chess in SFML");
     vector<vector <Square>> squareMatrix;
     Pawn p("white", 6 ,1); 
+    Rook r("white", 7, 0);
     buildMatrix(squareMatrix);
-
+    squareMatrix[6][1].p = p;
+    squareMatrix[7][0].p = r;
+    bool selectPhase = true;
+    Piece selectedPiece;
 
     while (window.isOpen()){
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed)
                 window.close();
         }
         window.clear();
+
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            if (selectPhase){
+                selectedPiece = selectPiece(squareMatrix, window);
+                if (selectedPiece.color == "white" || selectedPiece.color == "black"){
+                    selectPhase = false;
+                }
+            }
+            else{
+                movePiece(squareMatrix, window, selectedPiece);
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+            selectPhase = true;
+        }
+
         drawSquares(window, squareMatrix);
-        drawPieces(window, p);
+        drawPieces(window, squareMatrix);
         window.display();
     }
 
